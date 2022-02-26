a0290081ee63d48b0f9bab27a7d1e1d999496d5f
curious-ThinkPad <curious.ks.jp@gmail.com>
2022-02-26 20:10:03 +0900

    ボールつかみ機構の機能追加

--- nhk2022_launcher/scripts/accessories_controler.py
+++ nhk2022_launcher/scripts/accessories_controler.py
@@ -1,26 +1,31 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
+# from click import command
 import rospy
 
 from sensor_msgs.msg import Joy
-from std_msgs.msg import Bool
+# from std_msgs.msg import Bool
+from std_msgs.msg import Float32MultiArray
 
 class Accessories_Controller():
     def __init__(self):
         self.joy_sub = rospy.Subscriber("joy", Joy, self.Joycallback)
-        self.ball_catcher_vertical= rospy.Publisher('/ball_catcher_vertical', Bool ,queue_size=1)
-        self.ball_catcher_catch=rospy.Publisher('/ball_catcher', Bool ,queue_size=1)
+        self.accessories_controler_pub= rospy.Publisher('/accessories_controler_commands', Float32MultiArray ,queue_size=1)
         self.ball_catcher_vertical_flag=False
         self.ball_catcher_catch_flag=False
+        self.pub_commands= Float32MultiArray()
 
     def Joycallback(self, msg):
         if msg.buttons[6]:#LT
-            self.ball_catcher_vertical_flag=~self.ball_catcher_vertical_flag
-            self.ball_catcher_vertical.publish(self.ball_catcher_vertical_flag)
+            self.ball_catcher_vertical_flag= not self.ball_catcher_vertical_flag
+            rospy.loginfo("ball catcher hight changed")
         
         elif msg.buttons[2]:#B
-            self.ball_catcher_catch_flag=~self.ball_catcher_catch_flag
-            self.ball_catcher_catch.publish(self.ball_catcher_catch_flag)
+            self.ball_catcher_catch_flag= not self.ball_catcher_catch_flag
+            rospy.loginfo("ball catcher clip changed")
+        
+        self.pub_commands.data = [float(self.ball_catcher_catch_flag), float(self.ball_catcher_vertical_flag)]
+        self.accessories_controler_pub.publish(self.pub_commands)
 
 
 if __name__ == '__main__':

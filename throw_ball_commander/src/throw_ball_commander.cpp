d0cb5d6abc96608e6a1ae9c939cd35987071ed16
long-paddy-field <4272166+long-paddy-field@users.noreply.github.com>
2022-04-08 16:12:53 +0900

    nya

--- throw_ball_commander/src/throw_ball_commander.cpp
+++ throw_ball_commander/src/throw_ball_commander.cpp
@@ -1,9 +1,9 @@
 #include "throw_ball_commander.h"
 
-Throw_Ball_Commander::Throw_Ball_Commander(ros::NodeHandle &_nh,int &_loop_rate, int &_lost_time_threshold, 
-                        float &_neck_length, float & _rise_rate,float &_motor_click, float &_luck_click)
-:nh(_nh),loop_rate(_loop_rate),lost_time_threshold(_lost_time_threshold),
- neck_length(_neck_length),rise_rate(_rise_rate),motor_click(_motor_click),luck_click(_luck_click)
+Throw_Ball_Commander::Throw_Ball_Commander(ros::NodeHandle &_nh, int &_loop_rate, int &_lost_time_threshold,
+                                           float &_neck_length, float &_rise_rate, float &_motor_click, float &_luck_click)
+    : nh(_nh), loop_rate(_loop_rate), lost_time_threshold(_lost_time_threshold),
+      neck_length(_neck_length), rise_rate(_rise_rate), motor_click(_motor_click), luck_click(_luck_click)
 {
     ROS_INFO("Crating Throw_Ball_commander");
     ROS_INFO_STREAM("loop_rate: " << loop_rate);
@@ -13,15 +13,15 @@ Throw_Ball_Commander::Throw_Ball_Commander(ros::NodeHandle &_nh,int &_loop_rate,
     ROS_INFO_STREAM("motor_click: " << motor_click);
     ROS_INFO_STREAM("luck_click: " << luck_click);
 
-    sub_target = nh.subscribe("/target",1,&Throw_Ball_Commander::target_sub_callback,this);
-    sub_shot = nh.subscribe("/shot",1,&Throw_Ball_Commander::shot_flag_callback,this);
+    sub_target = nh.subscribe("/target", 1, &Throw_Ball_Commander::target_sub_callback, this);
+    sub_shot = nh.subscribe("/shot", 1, &Throw_Ball_Commander::shot_flag_callback, this);
     sub_emergency_stop = nh.subscribe("/emergency_stop_flag", 1, &Throw_Ball_Commander::emergency_stop_callback, this);
     last_sub_vel_time = std::chrono::system_clock::now();
 
     update();
 }
 
-//initializers
+// initializers
 void Throw_Ball_Commander::init_drivers()
 {
     rogi_link_msgs::RogiLink init_msg;
@@ -42,7 +42,7 @@ void Throw_Ball_Commander::init_drivers()
     init_msg.data[0] = 1;
     pub_ctrl.publish(init_msg);
 
-    init_msg.id = MZSV << 6 | 0x02;
+    init_msg.id = MZSV << 6 | INIT_CMD;
     init_msg.data[0] = 1;
     pub_ctrl.publish(init_msg);
 }
@@ -53,32 +53,52 @@ void Throw_Ball_Commander::init_variables()
     target_y = 0;
     target_distance = 0;
     target_theta = 0;
-    
+
+    roller_duty = 0;
+    neckUD_duty = 0;
+    neckRL_duty = 0;
+
     emergency_stop_flag = false;
     connection_flag = false;
     fire_flag = false;
 }
 
-//publishers
+// publishers
 void Throw_Ball_Commander::aim_commander()
 {
-
 }
 
 void Throw_Ball_Commander::shot_commander()
 {
-
+    rogi_link_msgs::RogiLink shoter;
+    shoter.id = MZSV << 6 | SHOT_CMD;
+    shoter.data[0] = 1;
+    pub_ctrl.publish(shoter);
 }
 
-//subscribers
-void Throw_Ball_Commander::target_sub_callback(const geometry_msgs::Twist::ConstPtr &cmd_vel)
+// subscribers
+void Throw_Ball_Commander::target_sub_callback(const geometry_msgs::Twist::ConstPtr &cmd_target)
 {
-    target_x = cmd_vel->linear.x;
-    target_y = cmd_vel->linear.y;
+    if (!emergency_stop_flag)
+    {
+        ROS_DEBUG("cmd_target recieved");
+        target_x = cmd_target->linear.x;
+        target_y = cmd_target->linear.y;
+        target_distance = target_x * target_x + target_y * target_y;
+        target_theta = atan(target_y / target_x);
 
-    target_distance = 0;
-    target_theta = 0;
+        last_sub_vel_time = std::chrono::system_clock::now();
+    }
+    else
+    {
+        target_x = 0;
+        target_y = 0;
+        target_distance = 0;
+        target_theta = 0;
 
+        last_sub_vel_time = std::chrono::system_clock::now();
+        ROS_ERROR_ONCE("throw_ball: emergency stopping...");
+    }
 }
 
 void Throw_Ball_Commander::shot_flag_callback(const std_msgs::Bool::ConstPtr &msg)
@@ -88,37 +108,35 @@ void Throw_Ball_Commander::shot_flag_callback(const std_msgs::Bool::ConstPtr &ms
 
 void Throw_Ball_Commander::emergency_stop_callback(const std_msgs::Empty::ConstPtr &msg)
 {
-    target_x = 0;
-    target_y = 0;
-    target_distance = target_x * target_x + target_y * target_y;
-    target_theta = atan(target_y/target_x);
-
     emergency_stop_flag = !emergency_stop_flag;
 }
 
 void Throw_Ball_Commander::ConnectionFlagCallback(const std_msgs::Bool::ConstPtr &msg)
 {
-    connection_flag= msg->data;
+    connection_flag = msg->data;
 }
-//caliculators
-void Throw_Ball_Commander::convert_theta_to_click()
+// caliculators
+void Throw_Ball_Commander::converter()
 {
-
 }
 
 void Throw_Ball_Commander::cal_aimming()
 {
-    
 }
 
-bool Throw_Ball_Commander::isSubscribed() {
+bool Throw_Ball_Commander::isSubscribed()
+{
     auto current_time = std::chrono::system_clock::now();
     const auto vel_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(
-                             current_time - last_sub_vel_time).count();
+                                 current_time - last_sub_vel_time)
+                                 .count();
 
-    if (vel_elapsed < lost_time_threshold) {
+    if (vel_elapsed < lost_time_threshold)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
@@ -137,7 +155,8 @@ void Throw_Ball_Commander::update()
 {
     ros::Rate r(loop_rate);
 
-    while(ros::ok() && connection_flag==false){
+    while (ros::ok() && connection_flag == false)
+    {
         ROS_WARN_ONCE("waiting for serial connection");
         ros::spinOnce();
         r.sleep();
@@ -147,12 +166,12 @@ void Throw_Ball_Commander::update()
     init_drivers();
     init_variables();
 
-    while(ros::ok())
+    while (ros::ok())
     {
-        if(isSubscribed())
+        if (isSubscribed())
         {
-
-        }else
+        }
+        else
         {
             reset();
         }
--- throw_ball_commander/src/throw_ball_commander.h
+++ throw_ball_commander/src/throw_ball_commander.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//include
+// include
 #include <ros/ros.h>
 #include <iostream>
 #include <chrono>
@@ -13,51 +13,61 @@
 #include "rogi_link_msgs/RogiLink.h"
 #include <math.h>
 
-
 // Hard IDs
-const char RRMD = 0x07;//Right Roller
-const char LRMD = 0x06;//Left Roller
-const char NKUD = 0x0F;//Neck Motor Up-Down
-const char NKRL = 0x10;//Neck Motor Right-Left
-const char MZSV = 0x0E;//First Servo Motor in the Magazine 
+const char RRMD = 0x07; // Right Roller
+const char LRMD = 0x06; // Left Roller
+const char NKUD = 0x0F; // Neck Motor Up-Down
+const char NKRL = 0x10; // Neck Motor Right-Left
+const char MZSV = 0x0E; // First Servo Motor in the Magazine
+
+// define shotter cmd
+enum Shotter
+{
+    INIT_CMD = 0x02,
+    SHOT_CMD = 0x20
+};
 
 class Throw_Ball_Commander
 {
-    public:
-    //Constructor & Destructor
-    Throw_Ball_Commander(ros::NodeHandle &_nh,int &_loop_rate, int &_lost_time_threshold,
-                        float &_neck_length, float & _rise_rate,float &_motor_click, float &_luck_click);
+public:
+    // Constructor & Destructor
+    Throw_Ball_Commander(ros::NodeHandle &_nh, int &_loop_rate, int &_lost_time_threshold,
+                         float &_neck_length, float &_rise_rate, float &_motor_click, float &_luck_click);
     ~Throw_Ball_Commander(){};
-    
-    private:
-    //Handlers
+
+private:
+    // Handlers
     ros::NodeHandle &nh;
 
-    //Publishers for gazebo    
+    // Publishers for gazebo
 
-    //Publishers for real
-    ros::Publisher pub_ctrl;//publisher for aiming target
+    // Publishers for real
+    ros::Publisher pub_ctrl; // publisher for aiming target
 
-    //Subscrivers
+    // Subscrivers
     ros::Subscriber sub_target;
     ros::Subscriber sub_shot;
     ros::Subscriber sub_emergency_stop;
-    //Configurations;
+    // Configurations;
     int loop_rate;
     int lost_time_threshold;
     float neck_length;
     float rise_rate;
-    float motor_click;//モーターのギア数
-    float luck_click;//ラックのギア数
+    float motor_click; //モーターのギア数
+    float luck_click;  //ラックのギア数
 
-    //variables;
+    // variables;
     float target_x;
     float target_y;
 
     float target_distance;
     float target_theta;
 
-    //flags
+    float roller_duty;
+    float neckUD_duty;
+    float neckRL_duty;
+
+    // flags
     bool emergency_stop_flag;
     bool connection_flag;
     bool fire_flag;
@@ -65,21 +75,20 @@ class Throw_Ball_Commander
     // Timers
     std::chrono::system_clock::time_point last_sub_vel_time;
 
-
-    //Methods
-    //initializers
+    // Methods
+    // initializers
     void init_drivers();
     void init_variables();
-    //publishers
+    // publishers
     void aim_commander();
     void shot_commander();
-    //subscribers
+    // subscribers
     void target_sub_callback(const geometry_msgs::Twist::ConstPtr &cmd_vel);
     void shot_flag_callback(const std_msgs::Bool::ConstPtr &msg);
     void emergency_stop_callback(const std_msgs::Empty::ConstPtr &msg);
     void ConnectionFlagCallback(const std_msgs::Bool::ConstPtr &msg);
-    //caliculators
-    void convert_theta_to_click();
+    // caliculators
+    void converter();
     void cal_aimming();
 
     bool isSubscribed();

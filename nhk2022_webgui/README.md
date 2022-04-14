9a7d92258453fc8043a3380b72f3da528432e1d1
Oyaki122 <oc.yaki122@gmail.com>
2022-04-14 17:30:48 +0900

    roslibjsをcommitで固定して動作検証完了

--- nhk2022_webgui/README.md
+++ nhk2022_webgui/README.md
@@ -7,3 +7,5 @@ install.shは実行している前提
 
 1. `roslanch rvizweb rvizweb.launch`
 1. `npm run serve`
+
+https://qiita.com/hbvcg00/items/2bc189727fb28ea84bd2#4-webworkify%E3%81%AE%E4%BF%AE%E6%AD%A3
--- nhk2022_webgui/src/App.vue
+++ nhk2022_webgui/src/App.vue
@@ -1,15 +1,85 @@
 <template>
   <div class="allContainer">
     <iframe src="http://localhost:8001/rvizweb/" class="rvizweb"></iframe>
-    <div class="mock"></div>
+    <div class="mock">
+      cmd_vel x: {{cmdX}}, y: {{cmdY}}
+      <p v-if="isConnected">Connected</p>
+      <p v-else>Disconnected</p>
+      <button @click="send">send!</button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script lang="ts" setup>
+import * as roslib from 'roslib'
+import { onMounted, ref } from 'vue'
 
-export default defineComponent({
-  name: 'App'
+const isConnected = ref<boolean>(false)
+const cmdX = ref<number>(0)
+const cmdY = ref<number>(0)
+
+const ros = new roslib.Ros({
+  url: 'ws://localhost:9090'
+})
+
+type cmdVelMsg = {
+  linear: {
+    x: number,
+    y: number,
+    z: number,
+  },
+  angular:{
+    x: number,
+    y: number,
+    z: number
+  }
+}
+
+const listener = new roslib.Topic<cmdVelMsg>({
+  ros,
+  name: '/cmd_vel',
+  messageType: 'geometry_msgs/Twist'
+})
+
+listener.subscribe(msg => {
+  cmdX.value = msg.linear.x
+  cmdY.value = msg.linear.y
+})
+
+function send () {
+  console.log('send')
+  const cmdVel = new roslib.Topic({
+    ros: ros,
+    name: '/cmd_vel',
+    messageType: 'geometry_msgs/Twist'
+  })
+  cmdVel.publish({
+    linear: {
+      x: 0.1,
+      y: 0.2,
+      z: 0.3
+    },
+    angular: {
+      x: -0.1,
+      y: -0.2,
+      z: -0.3
+    }
+  })
+}
+
+onMounted(() => {
+  ros.on('connection', () => {
+    console.log('Connected to websocket server.')
+    isConnected.value = true
+  })
+
+  ros.on('error', error => {
+    console.log('Error connecting to websocket server: ', error)
+  })
+
+  ros.on('close', () => {
+    console.log('Connection to websocket server closed.')
+  })
 })
 </script>
 

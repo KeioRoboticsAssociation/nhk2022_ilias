bd1cebd2f2a52cc32a16baa982f9e087c0809342
long-paddy-field <4272166+long-paddy-field@users.noreply.github.com>
2022-04-24 18:31:40 +0900

    getparamできるようになった。

--- throw_ball_commander/src/main.cpp
+++ throw_ball_commander/src/main.cpp
@@ -18,7 +18,14 @@ int main(int argc, char **argv)
     float motor_click = 36; //モーターのギア数
     float luck_click = 80;//ラックのギア数
 
+    pnh.getParam("loop_rate", loop_rate);
+    pnh.getParam("lost_time_threshold", lost_time_threshold);
+    pnh.getParam("neck_length", neck_length);
+    pnh.getParam("limit_UD", limit_UD);
+    pnh.getParam("limit_RL", limit_RL);
+    pnh.getParam("rise_rate", rise_rate);
+    pnh.getParam("roll_rate", roll_rate);
     Throw_Ball_Commander commander(nh, loop_rate, lost_time_threshold,
-                                   limit_UD, 
+                                   limit_UD,
                                    limit_RL, rise_rate, roll_rate, neck_length);
 }

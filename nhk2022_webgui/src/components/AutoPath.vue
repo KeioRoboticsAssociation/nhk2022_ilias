0046423a9ce2127d9a7dc52eeb0621db4458374e
Oyaki122 <oc.yaki122@gmail.com>
2022-05-12 14:55:46 +0900

    ダイアログ式に変更

--- nhk2022_webgui/src/components/AutoPath.vue
+++ nhk2022_webgui/src/components/AutoPath.vue
@@ -22,27 +22,57 @@ const cancelAction = () => {
   cancel.value();
 };
 
-const goalNum = ref<number>(0);
+const goalNum = ref<number>(1);
 const dirNum = ref<number>(0);
+
+const opensDialog = ref<boolean>(false);
 const send = () => {
   sendGoal({
     pathmode: goalNum.value,
     direction: dirNum.value,
   });
+  opensDialog.value = false;
 };
 </script>
 
 <template>
   <Card title="AutoPath">
-    <p>
-      path:
-      <q-slider v-model="goalNum" :min="1" :max="5" label label-always />
-    </p>
-    <p>
-      dir:
-      <q-slider v-model="dirNum" :min="0" :max="1" label label-always />
-    </p>
-    <q-btn @click="send">send!</q-btn>
+    <q-dialog v-model="opensDialog">
+      <q-card style="width: 80vw">
+        <q-card-section>
+          <div class="text-h6">パス選択</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <p>
+            path:{{ goalNum }}
+            <q-slider v-model="goalNum" :min="1" :max="5" label markers />
+          </p>
+          <p>Dir:</p>
+          <q-btn-toggle
+            v-model="dirNum"
+            toggle-color="primary"
+            :options="[
+              { label: '0', value: 0 },
+              { label: '1', value: 1 },
+            ]"
+          />
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat @click="send">send!</q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+    <q-btn
+      @click="
+        () => {
+          opensDialog = true;
+        }
+      "
+    >
+      パス設定
+    </q-btn>
     <p>{{ feedback }}</p>
     <p>{{ result }}</p>
     <p>{{ status }}</p>
--- nhk2022_webgui/src/pages/R2Auto.vue
+++ nhk2022_webgui/src/pages/R2Auto.vue
@@ -1,21 +1,21 @@
 <script lang="ts" setup>
 import CmdVel from "../components/CmdVel.vue";
+import AutoPath from "../components/AutoPath.vue";
 </script>
 
 <template>
   <q-page padding class="items-stretch row">
     <div class="col-12 row no-wrap justify-start items-start content-start">
-      <div class="col-4">
-        <p v-for="n in 1" :key="n">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil
-          praesentium molestias a adipisci, dolore vitae odit, quidem
-          consequatur optio voluptates asperiores pariatur eos numquam rerum
-          delectus commodi perferendis voluptate?
-        </p>
+      <div class="col-4" style="height: 100%">
+        <iframe
+          src="http://localhost:8001/rvizweb"
+          style="display: block; width: 100%; height: 100%"
+        ></iframe>
       </div>
       <div class="col column" style="height: 100%">
-        <div class="col-7">
+        <div class="col-7 row shelf">
           <CmdVel />
+          <AutoPath />
         </div>
         <div class="col">
           <p v-for="n in 3" :key="n">
@@ -30,4 +30,12 @@ import CmdVel from "../components/CmdVel.vue";
   </q-page>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.shelf {
+  * {
+    width: calc(98% / 3);
+    height: 98%;
+    margin: 0.1rem;
+  }
+}
+</style>

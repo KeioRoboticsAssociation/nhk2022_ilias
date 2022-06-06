8943f618bccdc1ca305dc59e3411787211a8d9a0
curious--jp <curious.ks.jp@gmail.com>
2022-06-06 20:13:08 +0900

    kaomangai

--- nhk2022_launcher/scripts/accessories_joy_controler_R1.py
+++ nhk2022_launcher/scripts/accessories_joy_controler_R1.py
@@ -137,11 +137,10 @@ class Rosconnector():
                 # self.controler_id=3
                 # rospy.loginfo("lagori catcher changed")
 
-            # if msg.buttons[11]:#Rightpush
-            #     # self.lagori_gripper_catch_flag = not self.lagori_gripper_catch_flag
-            #     # self.accessories_pub_commands.data = [float(3), float(self.lagori_gripper_catch_flag)]
-            #     # self.controler_id=3
-                # rospy.loginfo("lagori catcher changed")
+            if msg.buttons[11]:#Rightpush
+                self.teleop_flag = not self.teleop_flag
+                self.teleop_flag_pub.publish(self.teleop_flag)
+                rospy.logwarn("teleop_flag changed %s",self.teleop_flag)
 
 
     def msg_gen(self, msg):
